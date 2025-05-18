5b298e67fbf55d7f98a08de6360bb9e7ee462fc6
Volodymyr Partyka <vovuk097@gmail.com>
2025-05-18 21:45:39 +0300

    Requests Functionality Notifications, Add Accept & Reject

--- frontend/src/pages/notifications/index.jsx
+++ frontend/src/pages/notifications/index.jsx
@@ -10,6 +10,7 @@ import useUserStore from "@/stores/useUserStore";
 import { REQUEST_STATUSES, REQUEST_STATUSES_VERBS } from "@/models/request";
 
 import RootNavigation from "@/components/Nav/Nav";
+import SecondaryBtn from "@/components/buttons/SecondaryBtn";
 
 export default function Notifications() {
   const supabase = useSupabaseClient();
@@ -39,43 +40,45 @@ export default function Notifications() {
     fetchMarkAllAsRead();
   }, [user]);
 
+  const fetchRequests = async () => {
+    setLoading(true);
+
+    const { data: requests, error } = await supabase
+      .from("requests")
+      .select(
+        `
+          *,
+          requester:requester_id (id, name, email),
+          assigner:assigner_id (id, name, email)
+      `
+      )
+      .or(`requester_id.eq.${user.id},assigner_id.eq.${user.id}`)
+      .order("created_at", { ascending: false });
+
+    const groupIds = [...new Set(requests.filter((e) => e.target_type === "groups").map((e) => e.target_id))];
+    const { data: groups } = await supabase.from("groups").select("id, title").in("id", groupIds);
+    const groupsMap = Object.fromEntries((groups || []).map((e) => [e.id, e]));
+
+    const _data = requests.map((e) => ({
+      ...e,
+      group: e.target_type === "groups" ? groupsMap[e.target_id] : {},
+    }));
+
+    if (error) {
+      setError("Error fetching requests");
+      console.error("Error fetching requests:", error);
+    } else {
+      setRequests(_data);
+    }
+
+    setLoading(false);
+  };
+
   useEffect(() => {
     if (!user) return;
 
-    const fetchRequests = async () => {
-      const { data: requests, error } = await supabase
-        .from("requests")
-        .select(
-          `
-            *,
-            requester:requester_id (id, name, email),
-            assigner:assigner_id (id, name, email)
-        `
-        )
-        .or(`requester_id.eq.${user.id},assigner_id.eq.${user.id}`)
-        .order("updated_at", { ascending: false });
-
-      const groupIds = [...new Set(requests.filter((e) => e.target_type === "groups").map((e) => e.target_id))];
-      const { data: groups } = await supabase.from("groups").select("id, title").in("id", groupIds);
-      const groupsMap = Object.fromEntries((groups || []).map((e) => [e.id, e]));
-
-      const _data = requests.map((e) => ({
-        ...e,
-        group: e.target_type === "groups" ? groupsMap[e.target_id] : {},
-      }));
-
-      if (error) {
-        setError("Error fetching requests");
-        console.error("Error fetching requests:", error);
-      } else {
-        setRequests(_data);
-      }
-
-      setLoading(false);
-    };
-
     fetchRequests();
-  }, [loading, user]);
+  }, [user]);
 
   const userDisplayName = (person) =>
     person.id === user.id ? (
@@ -105,9 +108,49 @@ export default function Notifications() {
     return `for ${targetName} - ${request.group.title}`;
   };
 
+  const handleAction = async (request, action) => {
+    setLoading(true);
+
+    const approve = async () => {
+      const { data: newRequest, error } = await supabase
+        .from("requests")
+        .insert({
+          requester_id: request.requester_id,
+          assigner_id: request.assigner_id,
+          target_id: request.target_id,
+          target_type: request.target_type,
+          status: action,
+        })
+        .select("*")
+        .maybeSingle();
+      if (error) {
+        console.error("Error processing request:", error);
+      }
+      return newRequest;
+    };
+    const updateCurrentRequest = async (newRequest) => {
+      const { error } = await supabase
+        .from("requests")
+        .update({ action_request_id: newRequest.id })
+        .eq("id", request.id);
+
+      if (error) {
+        console.error("Error updating current request:", error);
+      }
+    };
+
+    const newRequest = await approve();
+    if (newRequest) {
+      await updateCurrentRequest(newRequest);
+    }
+
+    setLoading(false);
+    fetchRequests();
+  };
+
   return (
     <div>
-      <RootNavigation title="Notifications" backBtn={true} />
+      <RootNavigation title="Notifications" backBtn />
 
       <div className="pt-12">
         {loading && (
@@ -128,7 +171,7 @@ export default function Notifications() {
             )}
             <Stack sx={{ width: "100%" }} spacing={1}>
               {requests.map((request) => (
-                <Alert key={request.id} variant="outlined" severity={requestSeverity(request)}>
+                <Alert key={request.id} severity={requestSeverity(request)}>
                   <p>
                     {request.requester.id === user.id && request.status === REQUEST_STATUSES.PENDING && (
                       <>
@@ -148,6 +191,20 @@ export default function Notifications() {
                       <>
                         {userDisplayName(request.requester)} {REQUEST_STATUSES_VERBS[request.status]} a request to{" "}
                         {userDisplayName(request.assigner)} {requestTarget(request)}
+                        {!request.action_request_id && (
+                          <div className="flex gap-2 mt-1 mb-1">
+                            <SecondaryBtn
+                              classes="!py-1 text-sm"
+                              title="Approve"
+                              onClick={() => handleAction(request, REQUEST_STATUSES.ACCEPTED)}
+                            />
+                            <SecondaryBtn
+                              classes="!py-1 text-sm"
+                              title="Reject"
+                              onClick={() => handleAction(request, REQUEST_STATUSES.REJECTED)}
+                            />
+                          </div>
+                        )}
                       </>
                     )}
 
@@ -158,7 +215,7 @@ export default function Notifications() {
                       </>
                     )}
                   </p>
-                  <p>Request Updated Date: {moment(request.updated_at).format("DD/MM/YYYY LT")}</p>
+                  <p>Request Date: {moment(request.created_at).format("DD/MM/YYYY LT")}</p>
                 </Alert>
               ))}
             </Stack>
